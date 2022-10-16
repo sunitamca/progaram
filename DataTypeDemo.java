9b2300794539aae96cfbef12dc9a293dff81f400
sunitamca <115772673+sunitamca@users.noreply.github.com>
2022-10-16 09:50:42 -0700

    Add files via upload
    14 oct java class

---
+++ DataTypeDemo.java
@@ -0,0 +1,14 @@
+//declaring a class
+class DataTypeDemo1
+{
+public static void main (string args[])
+{
+	//declaring local variables
+	int number1=89;
+	int number2=54;
+	int sum = number1+ number2;
+	System.out.printIn("The sum is :"+sum);
+}
+//end of main
+}
+//end of class DataTypeDemo
---
+++ DataTypeDemo2.java
@@ -0,0 +1,29 @@
+Program: To understand variables data types in java
+@author: sunita kumari
+@Data: 14 oct 2022
+*/
+
+//Declaring a class
+class DataTypeDemo2
+{
+public static void main(string args[])
+{
+	//delaring and initializing local variables
+	String name="Sunita Kumari";
+	char gender='F';
+	int age=25;
+	float marks=65.5f;
+	boolean pass=ture;
+	
+	//printing the values of all the variable
+	System.out.println("Name:"+ name);
+	System.out.println("Gender:"+ gender);
+	System.out.println("Age:"+ age);
+	System.out.println("Marks:"+ marks);
+	System.out.println("Pass:"+ pass);
+	
+}
+//end of main
+}
+
+//end of class DataTypeDemo2
